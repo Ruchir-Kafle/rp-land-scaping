dfcbc66cdee8d8efe1acca625cf628c7a0c85542
Ruchir <ruchirkafle@gmail.com>
2025-10-04 19:27:01 -0400

    carousel changes and code formatting mostly done

--- public/drake.js
+++ public/drake.js
@@ -1,7 +1,16 @@
 const carouselTemplate = document.querySelector(`template.carousel-template`);
-const starTemplate = document.querySelector(`template.star-template`);
 const reviewTemplate = document.querySelector(`template.review-template`);
 const serviceTemplate = document.querySelector(`template.service-template`);
+
+const star = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+stroke-linejoin="round" class="w-4 h-4 fill-current">
+    <polygon
+        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+    </polygon>
+</svg>
+`;
 
 const imagesPath = "./images/";
 
@@ -26,7 +35,6 @@ const services = {
     "Pressure Washing": "Driveways, Patios, Houses, Fences? No problem! We offer a wide range of pressure washing services.",
     "More!": "Got something else in mind? Just ask—we’re flexible and ready to help with most outdoor projects."
 }
-
 
 
 function carouselImageSwitching() {
@@ -87,6 +95,7 @@ function generateCarousels() {
     
 }
 
+
 function generateServices() {
     const serviceContainer = document.querySelector(`.services-container`);
     serviceContainer.innerHTML = ``;
@@ -118,9 +127,7 @@ function generateReviews() {
         cloneClient.innerHTML = "Client " + (reviewIndex + 1);
         
         for (let i = 0; i < 5; i++) {
-            const cloneStarTemplate = starTemplate.content.cloneNode(true);
-
-            cloneStarContainer.appendChild(cloneStarTemplate);
+            cloneStarContainer.innerHTML += star;
         }
 
         cloneQuote.innerHTML = review;
